53fe4380ff00a3fbeb4ffbbd38e69b82b56b29a1
Catalitium <catalitium@gmail.com>
2020-04-27 20:07:08 +0200

    Updates

---
+++ precache-manifest.8553d70ad372a88cb4ae20da494981f4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed5af143582df5127ca2fc2a81d3c5a",
+    "url": "/Catalitiumen/index.html"
+  },
+  {
+    "revision": "ba144ac43270e0c88d95",
+    "url": "/Catalitiumen/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8ef0bc38bc61dfda8677",
+    "url": "/Catalitiumen/static/css/main.9426a4e9.chunk.css"
+  },
+  {
+    "revision": "ba144ac43270e0c88d95",
+    "url": "/Catalitiumen/static/js/2.74d78179.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Catalitiumen/static/js/2.74d78179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef0bc38bc61dfda8677",
+    "url": "/Catalitiumen/static/js/main.a485d7d7.chunk.js"
+  },
+  {
+    "revision": "3ef811b59c0c80eda3b4",
+    "url": "/Catalitiumen/static/js/runtime-main.06170635.js"
+  },
+  {
+    "revision": "d5b97135b12132610ca02ff0b190df7c",
+    "url": "/Catalitiumen/static/media/AALogo.d5b97135.png"
+  },
+  {
+    "revision": "f6c033360089fcdf362aefb603729bf1",
+    "url": "/Catalitiumen/static/media/AIcircle-cropped.f6c03336.png"
+  },
+  {
+    "revision": "b4597ef3041410d11360b8d7e66616b8",
+    "url": "/Catalitiumen/static/media/AWSLogo.b4597ef3.png"
+  },
+  {
+    "revision": "8fab72c4f632b90b98b03d170d20bf46",
+    "url": "/Catalitiumen/static/media/Botcircle-cropped.8fab72c4.png"
+  },
+  {
+    "revision": "a1ef1c296b825df1cd0f3a36e4856563",
+    "url": "/Catalitiumen/static/media/Codecircle-cropped.a1ef1c29.png"
+  },
+  {
+    "revision": "59d81a18175fb47d28c4f2d9e94e1dee",
+    "url": "/Catalitiumen/static/media/InstagramLogo.59d81a18.png"
+  },
+  {
+    "revision": "239c240e6e23ecaa561943c7b905ef9d",
+    "url": "/Catalitiumen/static/media/MERNLogo.239c240e.png"
+  },
+  {
+    "revision": "75c36ef3660f11b4f12cfd4115f50bae",
+    "url": "/Catalitiumen/static/media/TwitterLogo.75c36ef3.png"
+  },
+  {
+    "revision": "295a70b12b112c701f54b95face09219",
+    "url": "/Catalitiumen/static/media/logo.295a70b1.png"
+  }
+]);
